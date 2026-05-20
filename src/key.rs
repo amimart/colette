49c858146a325d3f1dad71b11e8e533711f64c32
Arnaud Mimart <33665250+amimart@users.noreply.github.com>
2026-05-20 22:49:23 +0200

    feat: impl Key for ref and single tuple

--- src/key.rs
+++ src/key.rs
@@ -15,3 +15,27 @@ pub enum KeySize {
 pub trait HasKey<K: Key> {
     fn key(&self) -> K;
 }
+
+impl<K: Key + ?Sized> Key for &K {
+    const SIZE: KeySize = K::SIZE;
+
+    fn encode_into(&self, out: &mut Vec<u8>) {
+        (*self).encode_into(out);
+    }
+
+    fn encode(&self) -> Vec<u8> {
+        (*self).encode()
+    }
+}
+
+impl<K: Key + ?Sized> Key for (K,) {
+    const SIZE: KeySize = K::SIZE;
+
+    fn encode_into(&self, out: &mut Vec<u8>) {
+        self.0.encode_into(out);
+    }
+
+    fn encode(&self) -> Vec<u8> {
+        self.0.encode()
+    }
+}
