c4fee230705322e355f4fc4523a48e3e8a0f857b
Arnaud Mimart <33665250+amimart@users.noreply.github.com>
2026-05-20 23:24:11 +0200

    feat: implement IndexScan for non-prefixed idx keys

--- src/scan.rs
+++ src/scan.rs
@@ -1,5 +1,5 @@
 use std::marker::PhantomData;
-use std::ops::{Bound};
+use std::ops::{Bound, Range};
 use crate::entity::Entity;
 use crate::index::{Index, IndexKind, StoreKey};
 use crate::key::Key;
@@ -50,4 +50,42 @@ where
     after: Option<StoreKey<'a, Idx, PrimaryKey, Record>>,
 
     _marker: PhantomData<(PrimaryKey, Record, Idx)>,
+}
+
+impl<'a, ReadHandle, PrimaryKey, Record, Idx> IndexScan<'a, ReadHandle, PrimaryKey, Record, Idx>
+where
+    ReadHandle: MultiStoreReadHandle,
+    PrimaryKey: Key,
+    Record: Entity<PrimaryKey>,
+    Idx: Index<PrimaryKey, Record>,
+    Idx::Kind: IndexKind<Idx::Key, PrimaryKey>,
+{
+    pub fn new(read_handle: ReadHandle) -> Self {
+        Self {
+            read_handle,
+            range: ScanRange::All,
+            direction: Direction::LeftToRight,
+            after: None,
+
+            _marker: PhantomData,
+        }
+    }
+
+    pub fn range(mut self, range: Range<Bound<StoreKey<'a, Idx, PrimaryKey, Record>>>) -> Self {
+        self.range = ScanRange::Range {
+            left: range.start.map(|p |p.encode()),
+            right: range.end.map(|p |p.encode()),
+        };
+        self
+    }
+
+    pub fn direction(mut self, direction: Direction) -> Self {
+        self.direction = direction;
+        self
+    }
+
+    pub fn after(mut self, cursor: StoreKey<'a, Idx, PrimaryKey, Record>) -> Self {
+        self.after = Some(cursor);
+        self
+    }
 }
