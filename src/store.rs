3fc9584915a8fb4a99b1b78a9fc9606be668f758
Arnaud Mimart <33665250+amimart@users.noreply.github.com>
2026-05-21 01:20:57 +0200

    feat: add the Iter associated type to KVStore trait

--- src/store.rs
+++ src/store.rs
@@ -1,32 +1,25 @@
 use crate::error::BackendError;
+use crate::scan::{Direction, ScanRange};
 
 pub trait MultiStore {
-    type ReadHandle<'a>: MultiStoreReadHandle
-    where
-        Self: 'a;
-    type WriteHandle<'a>: MultiStoreWriteHandle
-    where
-        Self: 'a;
+    type ReadHandle: MultiStoreReadHandle;
+    type WriteHandle: MultiStoreWriteHandle;
 
-    fn read(&self) -> Result<Self::ReadHandle<'_>, BackendError>;
+    fn read(&self) -> Result<Self::ReadHandle, BackendError>;
 
-    fn write(&self) -> Result<Self::WriteHandle<'_>, BackendError>;
+    fn write(&self) -> Result<Self::WriteHandle, BackendError>;
 }
 
 pub trait MultiStoreReadHandle {
-    type Store<'a>: ReadKVStore
-    where
-        Self: 'a;
+    type Store: ReadKVStore;
 
-    fn open_store(&self, name: &str) -> Result<Self::Store<'_>, BackendError>;
+    fn open_store(&self, name: &str) -> Result<Self::Store, BackendError>;
 }
 
 pub trait MultiStoreWriteHandle {
-    type Store<'a>: ReadWriteKVStore
-    where
-        Self: 'a;
+    type Store: ReadWriteKVStore;
 
-    fn open_store(&mut self, name: &str) -> Result<Self::Store<'_>, BackendError>;
+    fn open_store(&mut self, name: &str) -> Result<Self::Store, BackendError>;
 
     fn commit(self) -> Result<(), BackendError>;
 }
@@ -40,5 +33,9 @@ pub trait WriteKVStore {
 }
 
 pub trait ReadKVStore {
+    type Iter: Iterator<Item = Result<(Vec<u8>, Vec<u8>), BackendError>>;
+
     fn get(&self, key: &[u8]) -> Result<Option<Vec<u8>>, BackendError>;
+
+    fn scan(&self, range: ScanRange, direction: Direction) -> Result<Self::Iter, BackendError>;
 }
