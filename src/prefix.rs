30556990e01c6c99dd7fead313519107ae33c400
Arnaud Mimart <33665250+amimart@users.noreply.github.com>
2026-05-20 22:48:49 +0200

    feat: impl a prefix helper to encode as end bound

--- src/prefix.rs
+++ src/prefix.rs
@@ -12,3 +12,14 @@ where
         self.encode()
     }
 }
+
+pub fn prefix_end(mut bytes: Vec<u8>) {
+    for i in (0..bytes.len()).rev() {
+        if bytes[i] != 0xff {
+            bytes[i] += 1;
+            bytes.truncate(i + 1);
+        }
+    }
+
+    bytes.push(0x00);
+}
