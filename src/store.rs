50963eeb11107697e69b4525b71ff07cf364a1aa
Arnaud Mimart <33665250+amimart@users.noreply.github.com>
2026-06-10 13:45:00 +0200

    feat: enforce static str for ns and store names

--- src/store.rs
+++ src/store.rs
@@ -5,21 +5,24 @@ pub trait MultiStore {
     type ReadHandle: MultiStoreReadHandle;
     type WriteHandle: MultiStoreWriteHandle;
 
-    fn read(&self, namespace: &str) -> Result<Self::ReadHandle, BackendError>;
+    fn read(&self, namespace: &'static str) -> Result<Self::ReadHandle, BackendError>;
 
-    fn write(&self, namespace: &str) -> Result<Self::WriteHandle, BackendError>;
+    /// Opens a MultiStoreWriteHandle for the given namespace. All writes to stores opened from this
+    /// handle will be atomic when commit() is called.
+    fn write(&self, namespace: &'static str) -> Result<Self::WriteHandle, BackendError>;
 }
 
 pub trait MultiStoreReadHandle {
     type Store: ReadKVStore;
 
-    fn open_store(&self, name: &str) -> Result<Self::Store, BackendError>;
+    fn open_store(&self, name: &'static str) -> Result<Self::Store, BackendError>;
 }
 
+/// A MultiStoreWriteHandle provides atomic writes across all stores opened from it.
 pub trait MultiStoreWriteHandle {
     type Store: ReadWriteKVStore;
 
-    fn open_store(&mut self, name: &str) -> Result<Self::Store, BackendError>;
+    fn open_store(&mut self, name: &'static str) -> Result<Self::Store, BackendError>;
 
     fn commit(self) -> Result<(), BackendError>;
 }
